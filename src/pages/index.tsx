563c251c02bd2b9659fcd77b1fc1f5390ee539fe
Waheed <a.waheed3738@gmail.com>
2023-02-18 12:59:58 +0500

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
       <>
 
-        <div className={'text-red-600 text-4xl bg-yellow-300'}>welcome</div>
+        <div className={'text-red-600 text-4xl bg-red-300'}>welcome</div>
         Not signed in <br/>
         <button onClick={() => signIn()}>Sign in</button>
       </>
